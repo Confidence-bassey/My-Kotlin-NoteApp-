0f644455129cceb4150e9b560046edb8e29adc94
Confidence-bassey <confidencebass2@gmail.com>
2022-06-16 15:43:07 +0100

    Made some corrections to courseInfo and title

--- app/src/main/java/com/confidenceb/noteappwithkotlin/DataManager.kt
+++ app/src/main/java/com/confidenceb/noteappwithkotlin/DataManager.kt
@@ -23,36 +23,36 @@ object DataManager {
     }
 
     private fun initializeNotes(){
-            var course = courses["android_intents"]!!
-            var note = NoteInfo(course, "Dynamic intent resolution",
+            var course = courses["android_intent"]!!
+            var note = NoteInfo(course, "Android Programming with Intents",
                     "Wow, intents allow components to be resolved at runtime")
             notes.add(note)
             note = NoteInfo(course, "Delegating intents",
                     "PendingIntents are powerful; they delegate much more than just a component invocation")
             notes.add(note)
 
-            course = courses["android_async"]!!
-            note = NoteInfo(course, "Service default threads",
+            course = courses["kotlin_android"]!!
+            note = NoteInfo(course, "Android Programming with Kotlin",
                     "Did you know that by default an Android Service will tie up the UI thread?")
             notes.add(note)
             note = NoteInfo(course, "Long running operations",
                     "Foreground Services can be tied to a notification icon")
             notes.add(note)
 
-            course = courses["java_lang"]!!
-            note = NoteInfo(course, "Parameters",
+            course = courses["android_java"]!!
+            note = NoteInfo(course, "Android Programming with Java",
                     "Leverage variable-length parameter lists")
             notes.add(note)
             note = NoteInfo(course, "Anonymous classes",
                     "Anonymous classes simplify implementing one-use types")
             notes.add(note)
 
-            course = courses["java_core"]!!
+           /* course = courses["java_core"]!!
             note = NoteInfo(course, "Compiler options",
                     "The -jar option isn't compatible with with the -cp option")
             notes.add(note)
             note = NoteInfo(course, "Serialization",
                     "Remember to include SerialVersionUID to assure version compatibility")
-            notes.add(note)
+            notes.add(note)  */
     }
 }
